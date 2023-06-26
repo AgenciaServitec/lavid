58a5dbb2d61320ff8510abe239683f5c143f651c
angelgt <angel.gamonal@agenciaservitec.com>
2023-06-26 12:58:19 -0500

    updated contact

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import styled from "styled-components";
-import {SvgEmail, SvgLocation, SvgPhone} from "../../images";
-import {mediaQuery} from "../../styles/constants/mediaQuery";
+import { SvgEmail, SvgLocation, SvgPhone } from "../../images";
+import { mediaQuery } from "../../styles/constants/mediaQuery";
 import Row from "antd/lib/row";
 import Col from "antd/lib/col";
 
 export const Contact = (onEventGaClickLink) => {
   return (
     <Container>
-      <Row gutter={[16,16]}>
+      <Row gutter={[16, 16]}>
         <Col xs={24} sm={24} md={12}>
           <div className="contact-icons">
             <h2 className="title-contacts">Contactanos</h2>
@@ -18,16 +18,16 @@ export const Contact = (onEventGaClickLink) => {
                   <SvgPhone width="80px" />
                 </div>
                 <div
-                    className="content"
-                    onClick={() =>
-                        onEventGaClickLink(
-                            "click-link-telefono-941801827",
-                            "Click link telefono 941801827"
-                        )
-                    }
+                  className="content"
+                  onClick={() =>
+                    onEventGaClickLink(
+                      "click-link-telefono-941801827",
+                      "Click link telefono 941801827"
+                    )
+                  }
                 >
-                  <a href="tel:+51941801827" target="_blank" rel="noreferrer">
-                    970 057 238
+                  <a href="tel:+51917605851" target="_blank" rel="noreferrer">
+                    917605851
                   </a>
                   <p> Lun-Dom 9am-6pm</p>
                 </div>
@@ -38,20 +38,20 @@ export const Contact = (onEventGaClickLink) => {
                   <SvgEmail width="80px" />
                 </div>
                 <div
-                    className="content"
-                    onClick={() =>
-                        onEventGaClickLink(
-                            "click-link-email-contactos@servitec-peru.com",
-                            "Click link email contactos@servitec-peru.com"
-                        )
-                    }
+                  className="content"
+                  onClick={() =>
+                    onEventGaClickLink(
+                      "click-link-email-contactos@servitec-peru.com",
+                      "Click link email contactos@servitec-peru.com"
+                    )
+                  }
                 >
                   <a
-                      href="mailto:contactos@servitec-peru.com"
-                      target="_blank"
-                      rel="noreferrer"
+                    href="mailto:contactos@servitecperu.com"
+                    target="_blank"
+                    rel="noreferrer"
                   >
-                    contactos@servitec-peru.com
+                    contactos@servitecperu.com
                   </a>
                   <p>Soporte en Linea</p>
                 </div>
@@ -62,18 +62,18 @@ export const Contact = (onEventGaClickLink) => {
                   <SvgLocation width="80px" />
                 </div>
                 <div
-                    className="content"
-                    onClick={() =>
-                        onEventGaClickLink(
-                            "click-link-ubicacion-EiSoB4t5tQ3rG5YU6",
-                            "Click link ubicacion https://goo.gl/maps/EiSoB4t5tQ3rG5YU6"
-                        )
-                    }
+                  className="content"
+                  onClick={() =>
+                    onEventGaClickLink(
+                      "click-link-ubicacion-EiSoB4t5tQ3rG5YU6",
+                      "Click link ubicacion https://goo.gl/maps/EiSoB4t5tQ3rG5YU6"
+                    )
+                  }
                 >
                   <a
-                      href="https://goo.gl/maps/EiSoB4t5tQ3rG5YU6"
-                      target="_blank"
-                      rel="noreferrer"
+                    href="https://goo.gl/maps/EiSoB4t5tQ3rG5YU6"
+                    target="_blank"
+                    rel="noreferrer"
                   >
                     Chorrillos, Perú
                   </a>
@@ -86,14 +86,14 @@ export const Contact = (onEventGaClickLink) => {
         <Col xs={24} sm={24} md={12}>
           <div className="contact-map">
             <iframe
-                title="map vida sana"
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3900.177213609453!2d-77.02684798496466!3d-12.168335347487432!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105b79b77b7175f%3A0xa6b195dde66ca32b!2sAv.%20Defensores%20del%20Morro%20479%2C%20Chorrillos%2015064!5e0!3m2!1ses-419!2spe!4v1671745777155!5m2!1ses-419!2spe"
-                width="auto"
-                height="450"
-                allowFullScreen=""
-                loading="lazy"
-                frameBorder={0}
-                referrerPolicy="no-referrer-when-downgrade"
+              title="map vida sana"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3900.177213609453!2d-77.02684798496466!3d-12.168335347487432!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105b79b77b7175f%3A0xa6b195dde66ca32b!2sAv.%20Defensores%20del%20Morro%20479%2C%20Chorrillos%2015064!5e0!3m2!1ses-419!2spe!4v1671745777155!5m2!1ses-419!2spe"
+              width="auto"
+              height="450"
+              allowFullScreen=""
+              loading="lazy"
+              frameBorder={0}
+              referrerPolicy="no-referrer-when-downgrade"
             />
           </div>
         </Col>
@@ -160,14 +160,14 @@ const Container = styled.div`
       }
     }
   }
-  
-  .contact-map{
+
+  .contact-map {
     width: 100%;
     height: 100%;
     display: flex;
     align-items: center;
     justify-content: center;
-    iframe{
+    iframe {
       width: 100%;
       height: 400px;
     }
